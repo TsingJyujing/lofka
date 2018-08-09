a38dfa8cad3a6c81f1fe1ab506ed84881bd28e1b
袁逸凡 <yuanyifan@cvnavi.com>
2018-08-09 13:33:13 +0800

    移除修复文件失效以后不需要的脚本

--- lofka-tail-py/watch_file.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ps -ef|grep lofka_tail|grep -v grep|awk '{print $2}'|xargs kill -9
-while :
-do
-	nohup python lofka_tail.py --app_name cvnavi/cvtsp/nginx --file /var/log/nginx/access.log --type nginx --target http://10.10.11.75:9500/ >>lofka_tail.log 2>&1 &
-	sleep 1200
-	ps -ef|grep lofka_tail|grep -v grep|awk '{print $2}'|xargs kill -9
-done
